9bb8a5cf6c5599603e324d35ef6460b41755b907
thunkar <gregojquiros@gmail.com>
2026-02-09 10:09:23 +0100

    keymap and drift

--- keyboards/euk/split/keymaps/default/keymap.c
+++ keyboards/euk/split/keymaps/default/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
     * |LShft |   Z  |   X  |   C  |   V  |   B  |                                  |   N  |   M  |  < , | > .  | ? /  | } ]  |
     * |------+------+------+------+------+------/                                  `------+------+------+------+------+------|
-    * | LCtl | LWin | LAlt | MO(2)| MO(1)|       ,------·------·    ,------·------·       | Entr | RWin | RAlt | Home | Del  |
+    * | LCtl |LCaps | LAlt | MO(2)| MO(1)|       ,------·------·    ,------·------·       | Entr | RWin | RAlt | Home | Del  |
     * `----------------------------------/       | Scr- | Scr+ |    | MB1  | MB2  |       \----------------------------------'
     *                                            |------+------|    |------+------|
     *                                            |Space |      |    | NUBS |Space |
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB  , KC_Q    ,  KC_W   ,  KC_E  ,  KC_R  ,  KC_T ,                                           KC_Y  ,  KC_U  ,  KC_I    , KC_O    , KC_P     ,  KC_LBRC ,
       KC_LGUI , KC_A    ,  KC_S   ,  KC_D  ,  KC_F  ,  KC_G ,                                           KC_H  ,  KC_J  ,  KC_K    , KC_L    , KC_SCLN  ,  KC_QUOT ,
       KC_LSFT , KC_Z    ,  KC_X   ,  KC_C  ,  KC_V  ,  KC_B ,                                           KC_N  ,  KC_M  ,  KC_COMM , KC_DOT  , KC_SLSH  ,  KC_RBRC ,
-      KC_LCTL , KC_LGUI ,  KC_LALT,  MO(2) ,  MO(1) ,                                                           KC_ENT ,  KC_RGUI , KC_RALT , KC_HOME  ,  KC_DEL  ,
+      KC_LCTL , KC_CAPS ,  KC_LALT,  MO(2) ,  MO(1) ,                                                           KC_ENT ,  KC_RGUI , KC_RALT , KC_HOME  ,  KC_DEL  ,
                                                             KC_BRID , KC_BRIU,        KC_MS_BTN1 , KC_MS_BTN2,
                                                             KC_SPC  , _______ ,       KC_NUBS , KC_SPC
     ),
